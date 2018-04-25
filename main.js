babc8def4e0e5738e0f3b335fa302b55bad8cca4
Benjie <benjiecodes@gmail.com>
2018-04-25 12:36:09 -0400

    added click and drag functionality to fire only when mouse is down

--- main.js
+++ main.js
@@ -1,7 +1,25 @@
+// painting board
 const canvas = document.querySelector("#paint");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+// medium and context of the board
 const ctx = canvas.getContext("2d");
 ctx.strokeStyle = "#BADA55";
 ctx.lineJoin = "round";
 ctx.lineCap = "round";
+
+// property to switch between drawing and not drawing
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+
+function draw(event) {
+  if (!isDrawing) return; // stop the function from running when mouse isn't clicked
+  console.log(event);
+}
+
+canvas.addEventListener("mousemove", draw);
+canvas.addEventListener("mousedown", () => (isDrawing = true));
+canvas.addEventListener("mouseup", () => (isDrawing = false));
+canvas.addEventListener("mouseout", () => (isDrawing = false));
