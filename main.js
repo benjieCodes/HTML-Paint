c085d9e4f2693e0f250d651883d41f9f816b66b3
Benjie <benjiecodes@gmail.com>
2018-04-25 12:26:32 -0400

    modified canvas context

--- main.js
+++ main.js
@@ -2,3 +2,6 @@ const canvas = document.querySelector("#paint");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 const ctx = canvas.getContext("2d");
+ctx.strokeStyle = "#BADA55";
+ctx.lineJoin = "round";
+ctx.lineCap = "round";
